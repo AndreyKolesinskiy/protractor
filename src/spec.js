97453bd8e93f1230af4cd3869b6edf076fc4d953
dexxtrometh <evgeny.yanul@gmail.com>
2017-10-31 21:31:19 +0300

    adding element to list

--- src/spec.js
+++ src/spec.js
@@ -1,8 +1,22 @@
 describe('angularjs homepage', function() {
     it('should greet the named user', function() {
         browser.get('http://www.angularjs.org');
+        
         element(by.model('yourName')).sendKeys('Julie');
+        
         var greeting = element(by.binding('yourName'));
         expect(greeting.getText()).toEqual('Hello Julie!');
+    });
+});
+
+describe('angularjs homepage', function() {
+    it('adding element to list', function() {
+        browser.get('http://www.angularjs.org');
+
+        element(by.model('todoList.todoText')).sendKeys('armaggeddon');
+        element(by.buttonText('add')).click();
+
+        var sum = element(by.binding('todoList.todos.length'));
+        expect(sum.getText()).toEqual('2 of 3 remaining');
     });
 });
