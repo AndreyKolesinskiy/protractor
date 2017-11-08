1e5c94937ba9686e956ffe28bf1b2148ab307be8
dexxtrometh <evgeny.yanul@gmail.com>
2017-11-08 18:12:40 +0300

    add comments

--- src/specs/input.e2e-spec.js
+++ src/specs/input.e2e-spec.js
@@ -3,10 +3,16 @@ var AngularHomePage = require('../pages/home.e2e-po.js');
 describe('angularjs homepage, input component', function() {
     var page = new AngularHomePage();
 
+    /**
+     * Get cell page.
+     */
     beforeEach(function () {
         browser.get('http://www.angularjs.org');
     });
 
+    /**
+     * Entered user's name must be outputted.
+     */
     it('should greed the named user', function() {
         page.nameInput.sendKeys('Julie');
         expect(page.greeting.getText()).toEqual('Hello Julie!');
--- src/specs/list.e2e-spec.js
+++ src/specs/list.e2e-spec.js
@@ -3,15 +3,24 @@ var AngularHomePage = require('../pages/home.e2e-po.js');
 describe('angularjs homepage, list component', function() {
     var page = new AngularHomePage();
 
+    /**
+     * Get cell page.
+     */
     beforeEach(function(){
         browser.get('http://www.angularjs.org');
     });
     
+    /**
+     * Entered element must be added to list.
+     * */
     it('should show entered element in list', function() {       
         page.search.sendKeys('AngularJS');
         expect(page.name.getText()).toEqual('AngularJS');
     });
 
+    /**
+     * Search results of other element must be fail.
+     * */
     it('should dont show entered element in list', function() {        
         page.search.sendKeys('Other');
         expect(page.name.isPresent()).toEqual(false);
