d8371b44feb57da9718e158983d372fe80c26a0e
dexxtrometh <evgeny.yanul@gmail.com>
2017-11-09 16:41:44 +0300

    add suites

--- src/conf.js
+++ src/conf.js
@@ -1,11 +1,20 @@
 exports.config = {
     framework: 'jasmine',
     seleniumAddress: 'http://localhost:4444/wd/hub',
-    specs: ['specs/**/*.e2e-spec.js'],
     capabilities: {
         browserName: 'chrome',
         chromeOptions: {
             args: ['--window-size=1360,768']
         }
+    },    
+    
+    specs: ['specs/**/*.e2e-spec.js'],    
+    
+    suites: {
+        home: ['specs/checkbox.e2e-spec.js',
+                'specs/input.e2e-spec.js',
+                'specs/list.e2e-spec.js'],
+        api: 'specs/multilevellist.e2e-spec.js',
+        productions: 'specs/productions.e2e-spec.js'
     }
 };
