9ef1f53c3b1a9079accd208084ec261044ff7763
dexxtrometh <evgeny.yanul@gmail.com>
2017-11-10 18:06:08 +0300

    set true fields after click on trees element

--- src/pages/productions.e2e-po.js
+++ src/pages/productions.e2e-po.js
@@ -5,4 +5,8 @@ function ProductionsPage() {
 
     that.productionsMenuElement = element(by.partialLinkText('Publikationspflege'));
     that.title = element(by.binding('applicationTitle'));
+    that.number = element(by.model('publication.name'));
+    that.type = element(by.model('publication.type'));
+    that.date = element(by.model('dateItem'));
+    that.price = element(by.model('publication.priceType'));
 }
--- src/specs/productions.e2e-spec.js
+++ src/specs/productions.e2e-spec.js
@@ -1,6 +1,6 @@
 var ProductionsPage = require('../pages/productions.e2e-po.js');
 
-describe('productions page', function() {
+describe('productions page', function () {
     var page = new ProductionsPage();
 
     /**
@@ -13,8 +13,26 @@ describe('productions page', function() {
     /**
      * Page's title must be like menu's selected element.
      */
-    it('should set title value like menus element', function() {
+    it('should set title value like menus element', function () {
         page.productionsMenuElement.click();
         expect(page.title.getText()).toEqual('Publikationspflege');
     });
+
+    /**
+     * Working shit. Set true fields after click on tree's element.
+     * */
+    it('should set true fields after click on trees element', function () {
+        element(by.id('content'))
+            .all(by.css('.aciTreePush')).get(18).click();
+        element(by.id('content'))
+            .all(by.css('.aciTreePush')).get(21).click();
+        element(by.id('content'))
+            .all(by.css('.aciTreeText')).get(22).click();
+
+        expect(page.number.getAttribute('value')).toBe('6556');
+        expect(page.type.getAttribute('value')).toBe('1');
+        expect(page.date.getAttribute('value')).toBe('02.03.2017');
+        expect(page.price.getAttribute('value')).toBe('0');
+    });
+
 });
