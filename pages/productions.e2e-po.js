f45ee6b8f26769bc68d0a9acc5ef84c5e1e98215
evgeny-yanul <evgeny.yanul@gmail.com>
2017-11-11 13:32:57 +0300

    refactoring of setting field's values after click

--- pages/productions.e2e-po.js
+++ pages/productions.e2e-po.js
@@ -1,6 +1,7 @@
 module.exports = ProductionsPage;
 
 function ProductionsPage() {
+    var EC = protractor.ExpectedConditions;
     var that = this;
 
     that.productionsMenuElement = element(by.partialLinkText('Publikationspflege'));
@@ -9,4 +10,16 @@ function ProductionsPage() {
     that.type = element(by.model('publication.type'));
     that.date = element(by.model('dateItem'));
     that.price = element(by.model('publication.priceType'));
+    
+    that.nod = element(by.tagName('body')).
+        element(by.cssContainingText('.aciTreeText', '40, Herbst/Winter 2015/2016'));
+    that.sub1 = element(by.tagName('body')).
+        element(by.cssContainingText('.aciTreeText', 'Prospekt'));
+    that.sub2 = element(by.tagName('body')).
+        element(by.cssContainingText('.aciTreeText', '6556 Schwarzpreis ET: 02.03.2017'));
+
+    that.visibilityWaitingAndDoubleClick = function (element) {
+        browser.wait(EC.visibilityOf(element), 6000);
+        browser.actions().doubleClick(element).perform();
+    };
 }
--- specs/productions.e2e-spec.js
+++ specs/productions.e2e-spec.js
@@ -2,7 +2,6 @@ var ProductionsPage = require('../pages/productions.e2e-po.js');
 
 describe('productions page', function () {
     var page = new ProductionsPage();
-    var EC = protractor.ExpectedConditions;
 
     /**
      * Get cell page.
@@ -14,33 +13,23 @@ describe('productions page', function () {
     /**
      * Page's title must be like menu's selected element.
      */
-    /*it('should set title value like menus element', function () {
+    it('should set title value like menus element', function () {
         page.productionsMenuElement.click();
         expect(page.title.getText()).toEqual('Publikationspflege');
-    });*/
+    });
 
     /**
-     * Working shit. Set true fields after click on tree's element.
+     * Click tree's element and test fields's values.
      * */
-    it('should set true fields after click on trees element', function () {
+    it('should set true values on fields after click on trees element', function () {
+        browser.actions().doubleClick(page.nod).perform();
 
-        var nod = element(by.id('content'))
-            .all(by.css('.aciTreePush')).get(18);
-        var sub1 = element(by.id('content'))
-            .all(by.css('.aciTreePush')).get(21);
-        var sub2 = element(by.id('content'))
-            .all(by.css('.aciTreeText')).get(22);
-
-        nod.click();
-        browser.wait(EC.visibilityOf(sub1), 5000);
-        sub1.click();
-        browser.wait(EC.visibilityOf(sub2), 5000);
-        sub2.click();
+        page.visibilityWaitingAndDoubleClick(page.sub1);
+        page.visibilityWaitingAndDoubleClick(page.sub2);
 
         expect(page.number.getAttribute('value')).toEqual('6556');
         expect(page.type.getAttribute('value')).toEqual('1');
         expect(page.date.getAttribute('value')).toEqual('02.03.2017');
         expect(page.price.getAttribute('value')).toEqual('0');
     });
-
 });
