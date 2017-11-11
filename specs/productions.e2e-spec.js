00cb675eeeb256ad4241c26382213f00a3175f53
evgeny-yanul <evgeny.yanul@gmail.com>
2017-11-11 12:59:14 +0300

    fix visibility bug

--- specs/productions.e2e-spec.js
+++ specs/productions.e2e-spec.js
@@ -2,6 +2,7 @@ var ProductionsPage = require('../pages/productions.e2e-po.js');
 
 describe('productions page', function () {
     var page = new ProductionsPage();
+    var EC = protractor.ExpectedConditions;
 
     /**
      * Get cell page.
@@ -13,26 +14,33 @@ describe('productions page', function () {
     /**
      * Page's title must be like menu's selected element.
      */
-    it('should set title value like menus element', function () {
+    /*it('should set title value like menus element', function () {
         page.productionsMenuElement.click();
         expect(page.title.getText()).toEqual('Publikationspflege');
-    });
+    });*/
 
     /**
      * Working shit. Set true fields after click on tree's element.
      * */
     it('should set true fields after click on trees element', function () {
-        element(by.id('content'))
-            .all(by.css('.aciTreePush')).get(18).click();
-        element(by.id('content'))
-            .all(by.css('.aciTreePush')).get(21).click();
-        element(by.id('content'))
-            .all(by.css('.aciTreeText')).get(22).click();
 
-        expect(page.number.getAttribute('value')).toBe('6556');
-        expect(page.type.getAttribute('value')).toBe('1');
-        expect(page.date.getAttribute('value')).toBe('02.03.2017');
-        expect(page.price.getAttribute('value')).toBe('0');
+        var nod = element(by.id('content'))
+            .all(by.css('.aciTreePush')).get(18);
+        var sub1 = element(by.id('content'))
+            .all(by.css('.aciTreePush')).get(21);
+        var sub2 = element(by.id('content'))
+            .all(by.css('.aciTreeText')).get(22);
+
+        nod.click();
+        browser.wait(EC.visibilityOf(sub1), 5000);
+        sub1.click();
+        browser.wait(EC.visibilityOf(sub2), 5000);
+        sub2.click();
+
+        expect(page.number.getAttribute('value')).toEqual('6556');
+        expect(page.type.getAttribute('value')).toEqual('1');
+        expect(page.date.getAttribute('value')).toEqual('02.03.2017');
+        expect(page.price.getAttribute('value')).toEqual('0');
     });
 
 });
