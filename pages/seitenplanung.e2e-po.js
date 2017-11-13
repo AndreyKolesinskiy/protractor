879a644668495df192d91a667e8d10b28c5d7029
evgeny-yanul <evgeny.yanul@gmail.com>
2017-11-13 10:38:17 +0300

    set title value like menus element - seitenplanung

--- pages/seitenplanung.e2e-po.js
+++ pages/seitenplanung.e2e-po.js
@@ -6,9 +6,24 @@ function SeitenplanungPage() {
 
     that.productionsMenuElement = element(by.partialLinkText('Publikationspflege'));
     that.title = element(by.binding('applicationTitle'));
+
+    that.nod = element(by.tagName('body')).
+        element(by.cssContainingText('.aciTreeText', '39, Frühling/Sommer 2015'));
+    that.sub1 = element(by.tagName('body')).
+        element(by.cssContainingText('.aciTreeText', 'Inszenierungspunkt'));
+    that.sub2 = element(by.tagName('body')).
+        element(by.cssContainingText('.aciTreeText', '3911 Schwarzpreis ET: 04.03.2016'));
+
+    that.menuElement = element(by.partialLinkText('EINKAUF'));
+    that.menuSubElement = element(by.partialLinkText('Seitenplanung'));
     
     that.visibilityWaitingAndClick = function (element) {
         browser.wait(EC.visibilityOf(element), 8000);
         browser.actions().click(element).perform();
+    };
+
+    that.visibilityWaitingAndDoubleClick = function (element) {
+        browser.wait(EC.visibilityOf(element), 6000);
+        browser.actions().doubleClick(element).perform();
     };
 }
--- specs/seitenplanung.e2e-spec.js
+++ specs/seitenplanung.e2e-spec.js
@@ -11,11 +11,24 @@ describe('stammdaten page', function () {
     });
 
     /**
-     * Page's title must be like menu's selected element.
+     * Page's title must be like menu's selected element - Publikationspflege.
      */
-    it('should set title value like menus element', function () {
+    it('should set title value like menus element - Publikationspflege', function () {
         page.productionsMenuElement.click();
         expect(page.title.getText()).toEqual('Publikationspflege');
     });
 
+    /**
+     * Page's title must be like menu's selected element - Seitenplanung.
+     * */
+    it('should set title value like menus element - Seitenplanung', function () {
+        page.visibilityWaitingAndDoubleClick(page.nod);
+        page.visibilityWaitingAndDoubleClick(page.sub1);
+        page.visibilityWaitingAndDoubleClick(page.sub2);
+
+        page.menuElement.click();
+        page.visibilityWaitingAndClick(page.menuSubElement);
+
+        expect(page.title.getText()).toEqual('Seitenplanung');
+    });
 });
