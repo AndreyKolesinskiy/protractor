2d56bb7c05a2be201e04091c07317464b6ab2a3a
evgeny-yanul <evgeny.yanul@gmail.com>
2017-11-15 19:01:58 +0300

    formatting

--- specs/stammdaten.e2e-spec.js
+++ specs/stammdaten.e2e-spec.js
@@ -18,12 +18,9 @@ describe('lab 2-3 - stammdaten page', function () {
     it('lab 2, step 2 - should set season and specific data in fields', function () {
         page.menuElement.click();
         util.visibilityWaitingAndClick(page.saisonsMenuSubElement);
-
         page.season.click();
-
         expect(page.identity.getAttribute('value')).toEqual('34');
         expect(page.name.getAttribute('value')).toEqual('Herbst/Winter 2012/2013');
-
         expect(page.startDate.getAttribute('value')).toEqual('01.09.2012');
         expect(page.endDate.getAttribute('value')).toEqual('28.02.2013');
     });
@@ -37,19 +34,16 @@ describe('lab 2-3 - stammdaten page', function () {
     it('lab 3, step 2 - should set name by selected element', function () {
         page.menuElement.click();
         util.visibilityWaitingAndClick(page.vorteileMenuSubElement);
-
         page.secondListItem.click();
         expect(page.name.getAttribute('value')).toEqual('VR_2');
     });
     
     it('lab 3, step 3 - should add new element', function () {
         page.menuElement.click();
-        util.visibilityWaitingAndClick(page.vorteileMenuSubElement);
-        
+        util.visibilityWaitingAndClick(page.vorteileMenuSubElement);        
         page.plusButton.click();
         page.popupNameField.sendKeys('Test_create');
         page.okButton.click();
-
         util.visibilityWaitingAndClick(page.firstListItem);
         expect(page.name.getAttribute('value')).toEqual('Test_create');
     });
@@ -57,12 +51,10 @@ describe('lab 2-3 - stammdaten page', function () {
     it('lab 3, step 4 - should edit element', function () {
         page.menuElement.click();
         util.visibilityWaitingAndClick(page.vorteileMenuSubElement);
-
         page.firstListItem.click();
         page.name.clear();
         page.name.sendKeys('Test_edit');
         page.saveButton.click();
-
         util.visibilityWaitingAndClick(page.firstListItem);
         expect(page.name.getAttribute('value')).toEqual('Test_edit');
     });
@@ -70,11 +62,9 @@ describe('lab 2-3 - stammdaten page', function () {
     it('lab 3, step 5 - should remove element', function () {
         page.menuElement.click();
         util.visibilityWaitingAndClick(page.vorteileMenuSubElement);
-
         util.visibilityWaitingAndClick(page.firstListItem);
         page.minusButton.click();
         page.yesButton.click();
-
         util.visibilityWaitingAndClick(page.firstListItem);
         expect(page.name.getAttribute('value')).toEqual('VR_1');
     });
