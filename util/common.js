4e4178d91a9a5164a1e1171b6962612e34bb99b1
evgeny-yanul <evgeny.yanul@gmail.com>
2017-11-15 16:52:50 +0300

    formatting utils

--- util/common.js
+++ util/common.js
@@ -43,8 +43,14 @@ function CommonUtil() {
 
     /* close branches  */
     that.closeBranch = function (node, subNodeLevel1) {
-        browser.actions().click(subNodeLevel1).sendKeys(protractor.Key.LEFT).perform();
-        browser.actions().click(node).sendKeys(protractor.Key.LEFT).perform();
+        browser.actions()
+            .click(subNodeLevel1)
+            .sendKeys(protractor.Key.LEFT)
+            .perform();
+        browser.actions()
+            .click(node)
+            .sendKeys(protractor.Key.LEFT)
+            .perform();
     };
     
     /* wait of visibility element and double click  */
@@ -56,19 +62,23 @@ function CommonUtil() {
     /* wait of visibility element and click  */
     that.visibilityWaitingAndClick = function (element) {
         browser.wait(EC.visibilityOf(element), 8000);
-        browser.actions().click(element).perform();
+        browser.actions()
+            .click(element)
+            .perform();
     };
     
     /* wait of visibility and select value in menu */
     that.changeElementMenu = function (element, value) {
         browser.wait(EC.visibilityOf(element), 8000);
         if (value == 'UP') {
-            browser.actions().click(element)
+            browser.actions()
+                .click(element)
                 .sendKeys(protractor.Key.ARROW_UP)
                 .sendKeys(protractor.Key.ENTER)
                 .perform();
         } else if (value == 'DOWN') {
-            browser.actions().click(element)
+            browser.actions()
+                .click(element)
                 .sendKeys(protractor.Key.ARROW_DOWN)
                 .sendKeys(protractor.Key.ENTER)
                 .perform();
