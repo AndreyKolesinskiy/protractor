10c6780e57335526593ca228888b150fed35daa7
evgeny-yanul <evgeny.yanul@gmail.com>
2017-11-29 12:38:27 +0300

    refactor locators. season data

--- po/specific/season/seasonData.js
+++ po/specific/season/seasonData.js
@@ -5,6 +5,6 @@ function SeasonData() {
 
     that.identity = element(by.model('item.identity'));
     that.name = element(by.model('item.name'));
-    that.startDate = element.all(by.model('dateItem')).first();
-    that.endDate = element.all(by.model('dateItem')).last();
+    that.startDate = element(by.cssContainingText('.row.smallspacer.col-def', 'Startdatum')).$('.form-control.ng-valid.ng-valid-required');
+    that.endDate = element(by.cssContainingText('.row.smallspacer.col-def', 'Enddatum')).$('.form-control.ng-valid.ng-valid-required');
 }
