8855be36a1ace513856d7d9ae012c4668bb97a9c
evgeny-yanul <evgeny.yanul@gmail.com>
2017-11-30 15:44:34 +0300

    refactor dropdown setter. publication data po

--- specs/lab-1.e2e-spec.js
+++ specs/lab-1.e2e-spec.js
@@ -1,24 +1,24 @@
 var util = require('../util/common.js'),
-data = require('../data/lab-1.e2e-data.json'),
+    data = require('../data/lab-1.e2e-data.json'),
 
-title = require('../po/common/title.js'),
-mainMenu = require('../po/common/mainMenu.js'),
-publicationTree = require('../po/specific/publication/publicationTree.js'),
+    title = require('../po/common/title.js'),
+    mainMenu = require('../po/common/mainMenu.js'),
+    publicationTree = require('../po/specific/publication/publicationTree.js'),
 
-PublicationData = require('../po/specific/publication/publicationData.js'),
-publicationData = new PublicationData();
+    PublicationData = require('../po/specific/publication/publicationData.js'),
+    publicationData = new PublicationData(data);
 
 describe('lab 1', function () {
 
     beforeAll(function () {
         util.loadPage();
     });
-    
+
     it('should set title value by menu element', function () {
         mainMenu.open(data.productionsMenuSubElement);
         expect(title.title.getText()).toEqual(data.productionsMenuSubElement);
     });
-    
+
     it('should set fields values after click on branch element', function () {
         publicationTree.selectBranchInnerNode(data.nodes);
         expect(publicationData.number.getAttribute('value')).toEqual(data.number);
@@ -26,12 +26,12 @@ describe('lab 1', function () {
         expect(publicationData.date.getAttribute('value')).toEqual(data.date);
         expect(publicationData.price.getAttribute('value')).toEqual(data.price);
     });
-    
+
     it('should set fields new values after click on trees element, rollback', function () {
         publicationData.setValue(publicationData.number, data.testNumber);
-        publicationData.setDropdownValueToUpper(publicationData.type, true);
+        publicationData.setType(data);
         publicationData.setValue(publicationData.date, data.testDate);
-        publicationData.setDropdownValueToUpper(publicationData.price, false);
+        publicationData.setPrice(data);
         expect(publicationData.number.getAttribute('value')).toEqual(data.testNumber);
         expect(publicationData.type.getAttribute('value')).toEqual(data.testType);
         expect(publicationData.date.getAttribute('value')).toEqual(data.testDate);
